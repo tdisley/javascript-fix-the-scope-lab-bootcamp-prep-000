61a22e77d08d8203258bc11aa8ae32fea9efe399
t dis <thomdisley@gmail.com>
2017-09-16 11:34:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function add2(n) {
   // Feel free to move things around!
 }
 
-var funkyFunction = function() {
+var funkyFunction = function test() {
   return function() {
     return "FUNKY!"
   }
